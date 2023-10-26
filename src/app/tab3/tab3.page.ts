d4c84912c71f8d18450f143a8f78a7f76067c286
KatarinaEgebjerg <katarina.egebjerg.gmail.com>
2023-10-26 10:29:24 +0200

    icons alignment

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  login: any = { username: 'Anna', password: '' };
 
+  
   constructor() {}
+  onLogin() {
+    console.log('user name:', this.login.username );
+    console.log('user password', this.login.password );
+  }
 
+  setLoginData() {
+    this.login.username = 'edupala.com';
+    this.login.password = '12345';
+  }
+  
 }

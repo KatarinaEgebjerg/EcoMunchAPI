4d3f1584b6e5e8163d56b255bd928ef019b58134
Siw <spersson@hotmail.com>
2023-10-27 21:29:09 +0200

    Tilføjet funktion til knappen ved click.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  isClicked: boolean = false;
 
   constructor() {}
+  
+  showMore() {
+  
+    // Toggle the 'isClicked' variable to true to trigger the CSS class change
+    this.isClicked = true;
 
+    // Add a setTimeout to reset the 'isClicked' variable after a short delay to remove the effect
+    setTimeout(() => {
+      this.isClicked = false;
+    }, 250); // Reset after 250 milliseconds (adjust as needed)
+  }
 }

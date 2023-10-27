daf8bff09d213ef1c8f9a3db0da3d70815b6f18d
KatarinaEgebjerg <katarina.egebjerg.gmail.com>
2023-10-27 10:32:13 +0200

    space

--- src/app/modal-popup/modal-popup.page.ts
+++ src/app/modal-popup/modal-popup.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 
+
 @Component({
   selector: 'app-modal-popup',
   templateUrl: './modal-popup.page.html',

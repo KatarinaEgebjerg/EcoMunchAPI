571d38428dc7cd546ddbe6c863fc863754f4aab9
Siw <spersson@hotmail.com>
2023-10-31 22:39:52 +0100

    Layout i home.tab i stedet for tab2. Show more, titel og searchbar tilføjet. Rettet i SCSS og funktion til knappen.

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -14,5 +14,17 @@ export class HomePage {
     this.navCtrl.navigateForward('tabs/home/profile');
   }
   
+  isClicked: boolean = false;
 
+  showMore() {
+  
+    // Toggle the 'isClicked' variable to true to trigger the CSS class change
+    this.isClicked = true;
+
+    // Add a setTimeout to reset the 'isClicked' variable after a short delay to remove the effect
+    setTimeout(() => {
+      this.isClicked = false;
+    }, 250); // Reset after 250 milliseconds (adjust as needed)
+  }
 }
+

dc4be71b39bd400797007368c71e1622489bb073
KatarinaEgebjerg <katarina.egebjerg.gmail.com>
2023-10-31 10:05:55 +0100

    implementering af favorite og unfavorite function

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth-service/auth.service';
+import { MealService } from '../services/meal-service/meal.service';
+import { UserService } from '../services/user-service/user.service';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +11,48 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
-
+  constructor(
+    private authService: AuthService,
+    private modalCtrl: ModalController,
+    private MealService: MealService,
+    private userService: UserService,
+  ) {}
+  user: any;
+  favorites: any[] = [];
+  ngOnInit() {
+    this.authService.currentUser.subscribe((data) => {
+      this.user = data;
+    });
+    console.log(this.user)
+    this.getFavorites();
+  }
+  
+  getFavorites() {
+    this.authService.currentUser.subscribe((user) => {
+      if (user) {
+        this.userService.getFavorites(user.uid).then((favorites) => {
+          this.favorites = favorites;
+        });
+      }
+    });
+  }
+  
+  async addFavorite() {
+    const mealId = '52855'; // replace with your static meal id
+  
+    await this.userService.addToFavorites(this.user.uid, mealId);
+    console.log('Meal added to favorites successfully');
+  }
+  
+  async removeFavorite() {
+    const mealId = '52855'; // Replace with the meal ID you want to remove from favorites
+  
+    try {
+      await this.userService.removeFromFavorites(this.user.uid, mealId);
+      console.log('Meal removed from favorites successfully');
+    } catch (error) {
+      console.error('Error removing meal from favorites:', error);
+    }
+  }
 }
+

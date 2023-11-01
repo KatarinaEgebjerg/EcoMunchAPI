373b572b27af0ffbf7251d18ede04f0ea0b4b85f
Asland3 <malthe3@outlook.com>
2023-11-01 22:51:21 +0100

    Added swiper

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,5 +1,5 @@
 import { IonicModule } from '@ionic/angular';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
@@ -9,5 +9,6 @@ import { HomePageRoutingModule } from './home-routing.module';
 @NgModule({
   imports: [IonicModule, CommonModule, FormsModule, HomePageRoutingModule],
   declarations: [HomePage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomePageModule {}
